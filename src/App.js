e33e2e421f873282e4a8d12ab5df776bd2bb7db3
farhanshahriyar <farhan.abir9999@gmail.com>
2023-06-09 02:26:17 +0600

    added reactrouterdom and rrd bs

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Headers from "./Components/Headers";
-import { Container } from "react-bootstrap"; //7:35
+import { Container } from "react-bootstrap"; 
 import Footer from "./Components/Footer";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Headers/>
       <main>
         <Container>
-          <HomeScreen/>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact/>
+          </Routes>
+          <Routes>
+            <Route path="/product/:id" element={<ProductScreen />}/>
+          </Routes>
         </Container>
       </main>
       <Footer/>
-    </> 
+    </Router>
   );
 }
 
