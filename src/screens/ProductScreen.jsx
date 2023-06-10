35d7b7ec5660a8b3289990aa2d07577de5fa7951
farhanshahriyar <farhan.abir9999@gmail.com>
2023-06-10 20:52:32 +0600

    fixed some bugs and added product ids and also products functionalities

--- src/screens/ProductScreen.jsx
+++ src/screens/ProductScreen.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
+import { Row,Col,Image,ListGroup,Card,Button } from "react-bootstrap";
+import Rating from '../Components/Rating';
+import products from '../products';
 
 const ProductScreen = () => {
+  const { id } = useParams()
+  const product = products.find((p) => String (p._id) === id);
+  // const product = products.find((p) => p._id === match.params.id);
   return (
     <div>
-      Product
+      {product.name}
     </div>
   )
 }
